069b1cd9b8114096d425fb2f8ecf70265520a018
Alexandre Araujo Moreira <alexandream@gmail.com>
2017-03-12 15:33:11 -0300

    Create representation for Fixnums (#5)
    Now we have immediate values in the form of fixnums. That complicates
    our tratment of values a bit and raises a few portability issues, but
    those will be dealt with in the future. For now, we have most of the
    types needed for a working machine.
    
    On with the primitive procedures now!

--- src/eval/values.c
+++ src/eval/values.c
@@ -4,6 +4,7 @@
 
 static NType _boolean_type;
 static NType _unknown_type;
+static NType  _fixnum_type;
 
 NValue N_TRUE;
 NValue N_FALSE;
@@ -30,34 +31,79 @@ ni_init_values() {
         n_destroy_error(&error);
         return -2;
     }
-	
-    N_TRUE = malloc(sizeof(NObject));
-    N_FALSE = malloc(sizeof(NObject));
-	N_UNKNOWN = malloc(sizeof(NObject));
 
-	if (!N_TRUE || !N_FALSE || !N_UNKNOWN) {
-		return -3;
-	}
+    n_construct_type(&_fixnum_type, "nuvm.Fixnum");
+    n_register_type(&_fixnum_type, &error);
+    if (!n_is_ok(&error)) {
+        n_destroy_error(&error);
+        return -2;
+    }
 
-	((NObject*)    N_TRUE)->type = &_boolean_type;
-	((NObject*)   N_FALSE)->type = &_boolean_type;
-	((NObject*) N_UNKNOWN)->type = &_unknown_type;
+    {
+        NObject *true_ptr    = malloc(sizeof(NObject));
+        NObject *false_ptr   = malloc(sizeof(NObject));
+        NObject *unknown_ptr = malloc(sizeof(NObject));
+
+        if (!true_ptr || !false_ptr || !unknown_ptr) {
+            return -3;
+        }
+
+        true_ptr->type = &_boolean_type;
+        false_ptr->type = &_boolean_type;
+        unknown_ptr->type = &_unknown_type;
+
+        N_TRUE = n_wrap_pointer(true_ptr);
+        N_FALSE = n_wrap_pointer(false_ptr);
+        N_UNKNOWN = n_wrap_pointer(unknown_ptr);
+    }
 
     return 0;
 }
 
 
-int n_is_boolean(NValue value) {
+NValue
+n_wrap_fixnum(NFixnum fixnum) {
+    return ((NValue) fixnum) << 1;
+}
+
+
+NValue
+n_wrap_pointer(NObject* pointer) {
+    return ((NValue) pointer) | 1;
+}
+
+
+NFixnum
+n_unwrap_fixnum(NValue value) {
+    return (NFixnum) (value >> 1);
+}
+
+
+NObject*
+n_unwrap_pointer(NValue value) {
+    return (NObject*) (value & ~1);
+}
+
+int
+n_is_boolean(NValue value) {
     return (n_eq_values(N_TRUE, value) || n_eq_values(N_FALSE, value));
 }
 
 
-int n_is_unknown(NValue value) {
+int
+n_is_unknown(NValue value) {
     return n_eq_values(N_UNKNOWN, value);
 }
 
+
+int
+n_is_fixnum(NValue value) {
+    return (value & 1) == 0;
+}
+
+
 NType*
 n_type_of(NValue value) {
-	return ((NObject*) value)->type;
+    return n_unwrap_pointer(value)->type;
 }
 
--- src/eval/values.h
+++ src/eval/values.h
@@ -1,19 +1,23 @@
 #ifndef N_E_VALUE_H
 #define N_E_VALUE_H
 
+#include "../common/compatibility/stdint.h"
 #include "types.h"
 
 typedef struct NObject NObject;
 
-typedef void* NValue;
+typedef intptr_t NValue;
+typedef short NFixnum;
 
 extern NValue N_TRUE;
 extern NValue N_FALSE;
 extern NValue N_UNKNOWN;
 
+#define N_FIXNUM_MIN ((NFixnum) -32768)
+#define N_FIXNUM_MAX ((NFixnum)  32767)
 
 struct NObject {
-	NType *type;
+    NType *type;
 };
 
 int
@@ -23,11 +27,26 @@ ni_init_values(void);
 
 #define n_unwrap_boolean(VAL) (VAL == N_TRUE ? 1 : 0)
 
+NValue
+n_wrap_fixnum(NFixnum fixnum);
+
+NValue
+n_wrap_pointer(NObject* pointer);
+
+NFixnum
+n_unwrap_fixnum(NValue value);
+
+NObject*
+n_unwrap_pointer(NValue value);
+
 int
 n_is_unknown(NValue value);
 
 int
 n_is_boolean(NValue value);
+
+int
+n_is_fixnum(NValue value);
 
 NType*
 n_type_of(NValue value);
